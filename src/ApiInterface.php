0e4c688869c29b60cf43e5d5407f14cffd1200b0
Vladimir Savritsky <vladimir.savritsky@gmail.com>
2023-01-08 22:48:36 +0300

    add absctract controller

--- src/ApiInterface.php
+++ src/ApiInterface.php
@@ -2,18 +2,19 @@
 
 namespace BitrixRestApi;
 
+use BitrixModels\Entity\BaseModel;
 use BitrixRestApi\Responser\Response\AbstractResponse;
 use Symfony\Component\HttpFoundation\Request;
 
 interface ApiInterface
 {
     public function setRequest(Request $request);
-    
+
     public function getRequest(): Request;
-    
-    public function setUser($user);
-    
-    public function getUser();
-    
+
+    public function setUser(BaseModel $user = null);
+
+    public function getUser(): BaseModel|null;
+
     public function response(AbstractResponse $response);
 }
--- src/Config.php
+++ src/Config.php
@@ -2,20 +2,20 @@
 
 namespace BitrixRestApi;
 
-use BitrixRestApi\AuthManager\JwtManager;
+use BitrixRestApi\Jwt\JwtManager;
 use BitrixRestApi\Responser\ResponserInterface;
 
 class Config
 {
     protected $format;
-    
+
     protected $authManager;
-    
+
     public function setFormat(ResponserInterface $responser)
     {
         $this->format = $responser;
     }
-    
+
     public function setAuthManager(JwtManager $manager)
     {
         $this->authManager = $manager;
